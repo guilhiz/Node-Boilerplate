f797df56b918c04911e07e067a515c2574a15aa9
guilhiz <guilhermevilelasobral@gmail.com>
2023-05-08 23:25:46 -0300

    chore: change strip-html-string by sanitize-html

--- src/utils/functions/sanitizeObject.ts
+++ src/utils/functions/sanitizeObject.ts
@@ -1,13 +1,12 @@
-import { stripHtml } from 'string-strip-html';
+import sanitizeHtml from 'sanitize-html';
 
 export const sanitizeObject = (object: Record<string, unknown>): Record<string, unknown> => {
   for (const key of Object.keys(object)) {
     if (typeof object[key] === 'string') {
-      object[key] = stripHtml(object[key] as string).result.trim();
+      object[key] = sanitizeHtml(object[key] as string).trim();
     } else if (typeof object[key] === 'object' && object[key] !== null) {
       object[key] = sanitizeObject(object[key] as Record<string, unknown>);
     }
   }
-
   return object;
 };
